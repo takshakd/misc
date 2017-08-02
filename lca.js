ff5160c4f64ddd7308152efe6f0a8b365385068a
Takshak Desai <takshakd@gmail.com>
2017-08-01 21:27:32 -0400

    Lowest common ancestor

--- lca.js
+++ lca.js
@@ -3,34 +3,55 @@ var treelib = require("./Tree");
 var Tree = treelib.Tree;
 var TreeNode = treelib.TreeNode;
 
-var finalPath = null;
-
-function getPathRec(path, node, t) {
-  if (node.left === null && node.right === null) {
-    if (node === t) {
-      // var finalPath = path.slice();
-      // finalPath.push(node.val);
-      // console.log(finalPath);
-      // return finalPath;
-      finalPath = path.slice();
-      finalPath.push(node.val);
-    } else {
-      console.log("Reached bad node");
-    }
-  } else {
-    path.push(node.val);
-    if (node.left !== null) {
-      return getPathRec(path.slice(), node.left, t);
-    }
-    if (node.right !== null) {
-      return getPathRec(path.slice(), node.right, t);
-    }
+function findPath(root, t, path) {
+  if (root === null) {
+    return false;
   }
+  if (root === t) {
+    path.push(root.val);
+    return true;
+  }
+  if (root.left !== null && findPath(root.left, t, path)) {
+    path.push(root.val);
+    return true;
+  }
+  if (root.right !== null && findPath(root.right, t, path)) {
+    path.push(root.val);
+    return true;
+  }
+  return false;
 }
 
 function lowestCommonAncestor(tree, a, b) {
-
+  var patha = [];
+  findPath(tree.root, a, patha);
+  patha.reverse();
+  var pathb = [];
+  findPath(tree.root, b, pathb);
+  pathb.reverse();
+  for (var i = 0; i < patha.length; i++) {
+    if (patha[i] !== pathb[i]) {
+      break;
+    }
+  }
+  return patha[i-1];
 }
+
+function lcaRec(root, a, b) {
+  if (root === null) {
+    return null;
+  }
+  if (root === a || root === b) {
+    return root;
+  }
+  var leftLca = lcaRec(root.left, a, b);
+  var rightLca = lcaRec(root.right, a, b);
+  if (leftLca !== null && rightLca !== null) {
+    return root;
+  }
+  return leftLca !== null ? leftLca : rightLca;
+}
+
 
 var n5 = new TreeNode(5, null, null);
 var n6 = new TreeNode(6, null, null);
@@ -38,8 +59,14 @@ var n4 = new TreeNode(4, null, null);
 var n3 = new TreeNode(3, n5, n6);
 var n2 = new TreeNode(2, null, null);
 var n1 = new TreeNode(1, n3, n4);
-var root = new TreeNode(0, n1, n2);
-var t = new Tree(root);
+var n0 = new TreeNode(0, n1, n2);
+var t = new Tree(n0);
 
-console.log(getPathRec([], t.root, n4));
-console.log(finalPath);
+console.log(lowestCommonAncestor(t, n5, n4));
+console.log(lcaRec(t.root, n5, n4).val);
+
+console.log(lowestCommonAncestor(t, n5, n6));
+console.log(lcaRec(t.root, n5, n6).val);
+
+console.log(lowestCommonAncestor(t, n5, n2));
+console.log(lcaRec(t.root, n5, n2).val);
